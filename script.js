ab48383b090aab4b11b937b99cd492fba9895664
Shiyas <166885292+nerdonyorks@users.noreply.github.com>
2026-05-03 00:26:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -96,7 +96,7 @@ document.addEventListener('DOMContentLoaded', () => {
      COUNTDOWN TIMER
      =============================== */
 
-  const weddingDate = new Date(2026, 4, 3, 11, 0, 0).getTime(); // April 29, 2026, 11:00 AM
+  const weddingDate = new Date(2026, 4, 17, 11, 0, 0).getTime(); 
 
   const timerInterval = setInterval(() => {
     const now = new Date().getTime();
